2f13628682c65023e860f365ab1e38cb0403a00f
Mohammed Mostakim Ornob <mohammed-mostakim.ornob@stud.uni-bamberg.de>
2020-01-04 13:45:11 +0100

    - Fix auth service

--- student-residence-application/services/authentication/service/src/main/java/org/authentication/service/services/interfaces/AuthenticationService.java
+++ student-residence-application/services/authentication/service/src/main/java/org/authentication/service/services/interfaces/AuthenticationService.java
@@ -1,19 +1,9 @@
 package org.authentication.service.services.interfaces;
 
-import org.authentication.common.exceptions.InvalidOperationException;
-import org.authentication.common.exceptions.ObjectNotFoundException;
-import org.authentication.common.exceptions.PaginationRangeOutOfBoundException;
-import org.authentication.common.exceptions.ValidationException;
-import org.authentication.dataaccess.data.models.Appointment;
-import org.authentication.dataaccess.data.models.User;
-import org.authentication.dataaccess.models.PaginatedDataList;
 import org.authentication.dataaccess.data.models.Authentication;
-import org.authentication.service.models.NewAppointment;
 import org.authentication.service.models.NewAuthentication;
 
 import javax.ws.rs.core.UriInfo;
-import java.time.LocalDate;
-import java.util.List;
 
 public interface AuthenticationService {
 Authentication addAuthenticatedUser(NewAuthentication newAuthentication);
