61d50ff57d7d8f3593d8bdb528515dcdc1477129
Mohammed Mostakim Ornob <mohammed-mostakim.ornob@stud.uni-bamberg.de>
2020-01-06 12:26:20 +0100

    - Remove unnecessary messages

--- student-residence-application/services/authentication/common/src/main/java/org/authentication/common/Messages.java
+++ student-residence-application/services/authentication/common/src/main/java/org/authentication/common/Messages.java
@@ -2,48 +2,14 @@ package org.authentication.common;
 
 public class Messages {
     public static final String INTERNAL_ERROR = "An internal error occurred.";
-    public static final String CONTRACT_ID_REQUIRED = "Contract ID is required.";
     public static final String REQUEST_BODY_REQUIRED = "Request body is required.";
-    public static final String CONTRACT_NOT_FOUND_WITH_ID = "No contract found with the specified contract ID.";
-    public static final String USER_NOT_AUTHORISED_TO_OPERATE_RESOURCE = "This User is not authorized to perform this operation on this resource.";
+    public static final String REQUIRED_USER_ID = "Valid User Id is required.";
+    public static final String REQUIRED_USER_EMAIL = "Valid User Email is required.";
+    public static final String REQUIRED_USER_PASSWORD = "Valid User Password is required.";
+    public static final String USER_NOT_FOUND="User Not Found With this ID.";
+    public static final String USER_MISMATCH="User Not Found With this ID and Password.";
 
-    public static final String SUCCESSFUL_CONFIRMATION = "Contract successfully confirmed.";
-    public static final String SUCCESSFUL_EXTENSION = "Contract successfully extended.";
-    public static final String SUCCESSFUL_TERMINATION = "Contract successfully terminated.";
-
-    public static final String REQUIRED_CONTRACTORS_NAME = "Contractor's Name is required.";
-    public static final String REQUIRED_CONTRACT_ID = "Contract ID is required.";
-    public static final String REQUIRED_ROOM_NUMBER = "Room Number is required.";
-    public static final String REQUIRED_APPOINTMENT_TYPE = "Valid Appointment Type is required";
-    public static final String REQUIRED_ISSUE = "Valid Issue is required.";
-    public static final String REQUIRED_APPOINTMENT_PRIORITY = "Valid Appointment Priority is required";
-    public static final String REQUIRED_DESIRED_DATE = "Desired Date is required.";
-    public static final String REQUIRED_APPOINTMENT_STATUS = "Valid Appointment Status is required";
-    public static final String REQUIRED_USER_ID = "Valid User Id is required";
-    public static final String REQUIRED_USER_EMAIL = "Valid User Email is required";
-    public static final String REQUIRED_USER_PASSWORD = "Valid User Password is required";
-    public static final String APPOINTMENT_NOT_FOUND="Appointment Not Found With this ID";
-    public static final String APPOINTMENT_ALREADY_ACCEPTED="Appointment is already accepted";
-    public static final String APPOINTMENT_DATE_EXPIRED="You need to accept appointment before desired date";
-    public static final String USER_NOT_FOUND="User Not Found With this ID";
-    public static final String USER_MISMATCH="User Not Found With this ID and Password";
-
-    public static final String INVALID_PAGE_NUM = "Invalid 'pageNum' value.";
-    public static final String INVALID_PAGE_SIZE = "Invalid 'pageSize' value.";
-    public static final String INVALID_EMAIL = "Valid Email is required.";
-    public static final String INVALID_PHONE = "Valid Phone Number is required.";
-    public static final String INVALID_END_DATE = "Valid End Date is required.";
-    public static final String INVALID_ROOM_NUMBER = "Valid Room Number is required.";
-    public static final String INVALID_CONTRACT = "Contract is not confirmed yet";
-    public static final String INVALID_DESIRED_DATE = "Appointment desired date should be between two weeks before start or end date";
-    public static final String INVALID_CONTRACTORS_USER_ID = "No User found with the specified User ID.";
-    public static final String INVALID_CONTRACTORS_USER_ROLE = "Contractor is not a resident.";
     public static final String INVALID_OR_EXPIRED_ACCESS_TOKEN = "Access Token is invalid/expired. Please login again to refresh the access token.";
 
-
-
     public static final String AUTHORIZATION_FAILED_USER_ROLE_NOT_ALLOWED = "This user's role is not allowed to perform this action.";
-    public static final String AUTHORIZATION_FAILED_CAUSE_NO_AUTHORIZATION_HEADER = "Authorization header must be provided";
-
-    public static final String PAGINATION_RANGE_EXCEEDS = "Pagination attributes exceeds data range.";
 }
