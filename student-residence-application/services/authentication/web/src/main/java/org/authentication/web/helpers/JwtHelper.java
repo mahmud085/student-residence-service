c69dea24dd6e4faaf05f8fb71a953a6a54782d67
Mohammed Mostakim Ornob <mohammed-mostakim.ornob@stud.uni-bamberg.de>
2020-01-13 13:41:12 +0100

    - Bug fix

--- student-residence-application/services/authentication/web/src/main/java/org/authentication/web/helpers/JwtHelper.java
+++ student-residence-application/services/authentication/web/src/main/java/org/authentication/web/helpers/JwtHelper.java
@@ -7,6 +7,9 @@ import org.authentication.common.exceptions.InvalidAccessTokenException;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class JwtHelper {
@@ -25,9 +28,14 @@ public class JwtHelper {
 
     public static void validateToken(String token) throws InvalidAccessTokenException {
         try {
-            Jwts.parser()
+            Claims claims = Jwts.parser()
                     .setSigningKey(getSigningKey())
                     .parseClaimsJws(token).getBody();
+            LocalDateTime expiresAt = LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(claims.get("exp").toString()) * 1000), ZoneId.systemDefault());
+
+            if (LocalDateTime.now().isAfter(expiresAt)) {
+                throw new InvalidAccessTokenException(Messages.INVALID_OR_EXPIRED_ACCESS_TOKEN);
+            }
         } catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
             throw new InvalidAccessTokenException(Messages.INVALID_OR_EXPIRED_ACCESS_TOKEN);
         }
