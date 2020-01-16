77de3800a9b2ab2a29e2fc90da7e7a8aa935f08d
Mohammed Mostakim Ornob <mohammed-mostakim.ornob@stud.uni-bamberg.de>
2020-01-16 18:18:05 +0100

    - Add new user

--- student-residence-application/database/authentication_db.sql
+++ student-residence-application/database/authentication_db.sql
@@ -27,7 +27,10 @@ insert into user (userId, userName, userEmail, password, userType)
 values 
 ('AU1', 'Admin User 1', 'admin.user1@domain.com', '123456', 'Admin'),
 ('AU2', 'Admin User 2', 'admin.user2@domain.com', '123456', 'Admin'),
+('AU3', 'Admin User 3', 'admin.user3@domain.com', '123456', 'Admin'),
 ('CU1', 'Caretaker User 1', 'caretaker.user1@domain.com', '123456', 'Caretaker'),
 ('CU2', 'Caretaker User 2', 'caretaker.user2@domain.com', '123456', 'Caretaker'),
+('CU3', 'Caretaker User 3', 'caretaker.user3@domain.com', '123456', 'Caretaker'),
 ('RU1', 'Resident User 1', 'resident.user1@domain.com', '123456', 'Resident'),
-('RU2', 'Resident User 2', 'resident.user2@domain.com', '123456', 'Resident');
+('RU2', 'Resident User 2', 'resident.user2@domain.com', '123456', 'Resident'),
+('RU3', 'Resident User 3', 'resident.user3@domain.com', '123456', 'Resident');
