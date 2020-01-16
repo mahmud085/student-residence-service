377a656b01cc45359f53d30852ed2b62e24c6873
Kowshik Dipta Das Joy <kowshik-dipta-das.joy@stud.uni-bamberg.de>
2020-01-16 23:12:49 +0100

    Added javadoc for appointment service

--- student-residence-application/services/appointment/data-access/src/main/java/org/appointment/dataaccess/respositories/interfaces/AppointmentRepository.java
+++ student-residence-application/services/appointment/data-access/src/main/java/org/appointment/dataaccess/respositories/interfaces/AppointmentRepository.java
@@ -7,13 +7,57 @@ import org.appointment.dataaccess.models.PaginatedDataList;
 
 import java.time.LocalDate;
 import java.util.List;
-
+/**
+ * AppointmentRepository is the repository interface for appointment.
+ * @author Tawhidul Islam Pritam
+ *
+ */
 public interface AppointmentRepository extends GenericRepository<Appointment> {
-    PaginatedDataList<Appointment> getAll(int pageNum, int pageSize);
+
+	/**
+	 * Retrieves appointments with pagination.
+	 * @param pageNum
+	 * @param pageSize
+	 * @return Paginated Data List of appointment.
+	 */
+	PaginatedDataList<Appointment> getAll(int pageNum, int pageSize);
+    /**
+     * Retrieves all appointments.
+     * @return List of  {@link Appointment}
+     */
     List<Appointment> getAll();
+    /**
+     * Retrieves all appointments filtered by desired date.
+     * @param desiredDateFilter
+     * @return List of  {@link Appointment} filtered by desired date.
+     */
     List<Appointment> getAll(LocalDate desiredDateFilter);
+    /**
+     * Retrieves appointments filtered by the given desired date with pagination.
+     * @param desiredDateFilter
+     * @param pageNum
+     * @param pageSize
+     * @return {@link PaginatedDataList} of all {@link Appointment}
+     */
     PaginatedDataList<Appointment> getAll(LocalDate desiredDateFilter, int pageNum, int pageSize);
+    /**
+     * Retrieve a single appointment
+     * @param appointmentId
+     * @return {@link Appointment}
+     * @throws ObjectNotFoundException
+     */
     Appointment getById(String appointmentId) throws ObjectNotFoundException;
+    /**
+     * Update Appointment status for a given appointment id.
+     * @param appointmentId
+     * @param status
+     * @throws ObjectNotFoundException
+     */
     void updateAppointmentStatus(String appointmentId, AppointmentStatus status) throws ObjectNotFoundException;
+    /**
+     * Retrieves all appointments created by a user.
+     * @param createdByUserId
+     * @return List of {@link Appointment}
+     */
     List<Appointment> getAll(String createdByUserId);
 }
--- student-residence-application/services/appointment/service/src/main/java/org/appointment/service/services/interfaces/AppointmentService.java
+++ student-residence-application/services/appointment/service/src/main/java/org/appointment/service/services/interfaces/AppointmentService.java
@@ -9,14 +9,81 @@ import org.appointment.service.models.NewAppointment;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * AppointmentService is the service interface for appointment.
+ * 
+ * @author Tawhidul Islam
+ *
+ */
 public interface AppointmentService {
+	
+	/**
+	 * This method creates new appointment
+	 * @param newAppointment
+	 * @param contract
+	 * @param contextUserId
+	 * @return {@link Appointment}
+	 * @throws ValidationException
+	 * @throws InvalidOperationException
+	 */
     Appointment createAppointment(NewAppointment newAppointment, Contract contract, String contextUserId) throws ValidationException, InvalidOperationException;
+    /**
+     * This method gives the {@link Appointment}
+     * @param appointmentId
+     * @return {@link Appointment}
+     * @throws ObjectNotFoundException
+     */
     Appointment getAppointment(String appointmentId) throws ObjectNotFoundException;
+    /**
+     * This method gives all appointment list
+     * @return {@link Appointment}
+     * @throws ObjectNotFoundException
+     */
     List<Appointment> getAllAppointments() throws ObjectNotFoundException;
+    /**
+     * Pagination management for appointment list
+     * @param pageNum
+     * @param pageSize
+     * @return {@link Appointment}
+     */
     PaginatedDataList<Appointment> getAllAppointments(int pageNum, int pageSize);
+    /**
+     * List of get all appointments
+     * @param desiredDate
+     * @return {@link java.util.List} of {@link Appoitment}
+     */
+    
     List<Appointment> getAllAppointments(LocalDate desiredDate);
+    /**
+     * Paginated list of get all appointments
+     * @param desiredDate
+     * @param pageNum
+     * @param pageSize
+     * @return {@link PaginatedDataList} of {@Link Appointment}
+     */
     PaginatedDataList<Appointment> getAllAppointments(LocalDate desiredDate, int pageNum, int pageSize);
+    /**
+     * This method is to accept appointment
+     * @param appointmentId
+     * @return {@link Appointment}
+     * @throws InvalidOperationException
+     * @throws ObjectNotFoundException
+     * @throws OperationAlreadyExecutedException
+     */
     Appointment acceptAppointment(String appointmentId) throws InvalidOperationException, ObjectNotFoundException, OperationAlreadyExecutedException;
+    /**
+     * This method is to deny appointment
+     * @param appointmentId
+     * @return {@link Appointment}
+     * @throws InvalidOperationException
+     * @throws ObjectNotFoundException
+     * @throws OperationAlreadyExecutedException
+     */
     Appointment denyAppointment(String appointmentId) throws InvalidOperationException, ObjectNotFoundException, OperationAlreadyExecutedException;
+    /**
+     * This method is to get appointment by contractors id
+     * @param contractorsUserID
+     * @return -list of appointments
+     */
     List<Appointment> getAppointmentsByContractor(String contractorsUserID);
 }
