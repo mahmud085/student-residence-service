8e57b5a4dde6c8f075dcc495151d9e4b3c9a54ba
Md imtiaz Abedin <md-imtiaz.abedin@stud.uni-bamberg.de>
2020-01-16 17:52:20 +0100

    Documentation added for authentication service

--- student-residence-application/services/authentication/data-access/src/main/java/org/authentication/dataaccess/respositories/interfaces/GenericRepository.java
+++ student-residence-application/services/authentication/data-access/src/main/java/org/authentication/dataaccess/respositories/interfaces/GenericRepository.java
@@ -4,8 +4,24 @@ import org.authentication.common.exceptions.ObjectNotFoundException;
 import org.authentication.dataaccess.data.models.Entity;
 
 import java.util.List;
-
+/**
+ * GenericRepository is the repository interface for generic entity.
+ *
+ * @author Imtiaz Abedin
+ *
+ */
 public interface GenericRepository<T extends Entity> {
+    /**
+     * Adds a entity.
+     * @param obj New entity object to be added.
+     * @return Added entity
+     */
     T add(T obj);
+
+
+    /**
+     * Retrieves all entities.
+     * @return List of all entities. Returns empty list if no entity is found.
+     */
     List<T> getAll();
 }
--- student-residence-application/services/authentication/service/src/main/java/org/authentication/service/services/interfaces/AuthenticationService.java
+++ student-residence-application/services/authentication/service/src/main/java/org/authentication/service/services/interfaces/AuthenticationService.java
@@ -5,7 +5,18 @@ import org.authentication.dataaccess.data.models.Authentication;
 import org.authentication.service.models.NewAuthentication;
 
 import javax.ws.rs.core.UriInfo;
-
+/**
+ * AuthenticationService is the repository interface for Authentication.
+ *
+ * @author Imtiaz Abedin
+ *
+ */
 public interface AuthenticationService {
+
+    /**
+     * Create authentication credentials.
+     * @param newAuthentication Object containing data for the authentication to be created.
+     * @return Created authentication.
+     */
     Authentication addAuthenticatedUser(NewAuthentication newAuthentication);
 }
